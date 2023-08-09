18d3821bebb50d74640616f50076e3820afe33a0
Andrii Halabuda <andrii.halabuda@gmail.com>
2023-08-09 14:50:24 -0400

    Project init

---
+++ voice-over/voice-over/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  voice-over
+//
+//  Created by Andrew on 2023-08-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
