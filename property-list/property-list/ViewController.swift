595781c1b61600ed038878e60ae3bfb04176c9d5
Andrii Halabuda <andrii.halabuda@gmail.com>
2023-08-10 23:22:19 -0400

    completed

--- property-list/property-list/ViewController.swift
+++ property-list/property-list/ViewController.swift
@@ -11,26 +11,48 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        guard let plistPath = Bundle.main.path(forResource: "tutorial-list", ofType: "plist"),
-              let plistData = FileManager.default.contents(atPath: plistPath) else {
-            return
-        }
-
-        do {
-            if let dictionary = try PropertyListSerialization.propertyList(from: plistData, options: [], format: nil) as? [String: Any] {
-                // You now have the plist data in the 'dictionary' variable.
-                // You can access specific values using dictionary[key].
-                
-                if let value = dictionary["number"] as? Int {
-                    print("Value for 'SomeKey': \(value)")
-                }
-            }
-        } catch {
-            print("Error loading plist: \(error)")
-        }
+        readPropertyList()
     }
 
-
+    private func readPropertyList() {
+        guard let path = Bundle.main.path(forResource: "tutorial-list", ofType: "plist") else {
+            print("Path for resource is incorrect")
+            return
+        }
+        
+        guard let data = FileManager.default.contents(atPath: path) else {
+            print("Data conversion failed")
+            return
+        }
+        
+        do {
+            let propertyList = try PropertyListSerialization.propertyList(from: data, format: nil)
+            guard let dict = propertyList as? [String: Any] else {
+                print("Dictionary casting failed")
+                return
+            }
+            
+            // Root
+            if let value = dict["number"] as? Int {
+                print("Number: \(value)")
+            }
+            
+            // Dictionary
+            if let dictionary = dict["dictionary-items"] as? [String: Any] {
+                if let item = dictionary["X1"] as? String {
+                    print("Dictionary item: \(item)")
+                }
+            }
+            
+            // Array
+            if let array = dict["array-items"] as? [Any] {
+                if let item = array[0] as? String {
+                    print("Array item: \(item)")
+                }
+            }
+        } catch let error {
+            print("Error serializing plist: \(error.localizedDescription)")
+        }
+    }
 }
 
