f838c2aa7a0858ac79ca0dff520b2b873d439ee4
Andrii Halabuda <andrii.halabuda@gmail.com>
2023-09-02 17:03:35 -0400

    Updated tutorial

--- dynamic-font-swiftui/dynamic-font-swiftui/ContentView.swift
+++ dynamic-font-swiftui/dynamic-font-swiftui/ContentView.swift
@@ -23,49 +23,32 @@ extension View {
     }
 }
 
-extension UIFont {
-    static let largeFont = UIFont.systemFont(ofSize: 16, weight: .light)
-    static let largeFont2 = UIFont.preferredFont(forTextStyle: .callout)
-}
-
 extension Font {
-    static let largeSemibold2 = Font.system(size: 16, weight: .semibold, design: .default)
-    static let largeSemibold = Font.system(.callout, design: .default, weight: .semibold)
-    static let superLarge = Font.custom("", size: 30, relativeTo: .title)
-    static let superLarge2 = Font.custom(UIFont().familyName, size: 30, relativeTo: .title)
+    static let titleSemibold = Font.system(.title, design: .default, weight: .semibold)
+    static let title30Semibold = Font.system(size: 30, weight: .semibold, design: .default)
 }
 
 struct ContentView: View {
     var body: some View {
         VStack(spacing: 20) {
-            Text("Hello, Title!")
+            Text("Large Title!")
                 .font(.largeTitle)
+            Text("Bold Title!")
+                .font(.title)
                 .bold()
-            Text("Hello, Custom Title!")
-                .font(.superLarge)
-            Text("Hello, Custom name Title!")
-                .font(.superLarge)
-            Text("Hello, regular text!")
-                .font(.callout)
-            Text("Hello, non dynamic regular text!")
+            Text("Semibold Title!")
+                .font(.titleSemibold)
+            Text("Static Title size 30!")
+                .font(.title30Semibold)
+            Text("Dynamic SF Pro light text size 24")
+                .scaledFont(size: 24, weight: .light)
+            Text("Dynamic Helvetica thin text size 16")
                 .scaledFont(name: "Helvetica", size: 16, weight: .thin)
+            Text("Dynamic SF Pro light text size 20 restricted")
+                .scaledFont(size: 20, weight: .light)
                 .dynamicTypeSize(...DynamicTypeSize.xxxLarge)
-            Text("Hello, UIFont regular text!")
-                .font(.custom(UIFont.largeFont.familyName, size: UIFont.largeFont.pointSize))
-                .fontWeight(.bold)
-                .dynamicTypeSize(...DynamicTypeSize.xxxLarge)
-            Text("Hello, regular bold text!")
-                .font(.callout)
-                .bold()
-            Text("Hello, regular semibold text!")
-                .font(.largeSemibold)
         }
         .padding()
-        .onAppear {
-            print(UIFont.systemFont(ofSize: 12).fontName)
-            print(UIFont.systemFont(ofSize: 12).familyName)
-            print(UIFont.systemFont(ofSize: 12).fontDescriptor.fontAttributes)
-        }
     }
 }
 
